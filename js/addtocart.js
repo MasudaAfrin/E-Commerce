6eb5614d0c8683a9a9fc7f9058596574f0026391
MasudaAfrin <masudatuba@gmail.com>
2020-08-21 01:11:11 +0600

    third commit

--- js/addtocart.js
+++ js/addtocart.js
@@ -1,6 +1,6 @@
 //get the products added to the cart
 let carts = document.querySelectorAll('.add-cart');
-//let demo = [];
+
 //static data which will be added to the cart
 let products = [
     {
@@ -22,11 +22,11 @@ let products = [
         inCart: 0 
     }
 ];
-//demo.push(products[0]);
-//console.log(demo[0]);
+
 //adding click event to the products
 for(let i=0; i<carts.length; i++){
     carts[i].addEventListener('click',()=>{
+        alert("Successfully added to cart");
         cartNumbers(products[i]);
         totalCost(products[i]);
     });
@@ -93,16 +93,20 @@ function totalCost(product){
 function displayCart(){
     let getProducts = localStorage.getItem("productInCart");
     getProducts = JSON.parse(getProducts);
-    
-    //console.log(getProducts);
+    var i = 0;
+    var j = 0;
+    var k = 0;
     let productContainer = document.querySelector('.products-incart');
     if(getProducts && productContainer){
+        
         productContainer.innerHTML = '';
         Object.values(getProducts).map(item =>{
+            
             productContainer.innerHTML += `
             <div class = "product-header">
             <div class = "product">
-                <i class="fas fa-times"></i>
+                <a href="#" onclick = "deleteitems(${i++})"><i class="fas fa-times"></i></a>
+                
                 <img src="./images/${item.tag}.jpg" width = "60px" height= "50px">
                 <span>${item.name}</span>
             </div>
@@ -111,20 +115,140 @@ function displayCart(){
                 <span>tk</span>
             </div>
             <div class="product-quantity">
-                 <i class="fas fa-chevron-circle-up"></i>
-                <span style="padding: 0px 10px; border:1px solid black; border-radius:5px">${item.inCart}</span>
-                <i class="fas fa-chevron-circle-down"></i>
+                <a href="#" onclick = "decreaseOne(${j++})"><i class="fas fa-chevron-circle-down"></i></a>
+                <span id = "quan-number" style="padding: 0px 10px; border:1px solid black; border-radius:5px">${item.inCart}</span>
+                <a href="#" onclick = "increaseOne(${k++})" id = "inc"><i class="fas fa-chevron-circle-up"></i></a>
             </div>
             <div class="product-total">
                 ${item.price * item.inCart}
                 <span>tk</span>
             </div>
             </div>
+        ` 
+        });      
+    }   
+    else{
+        productContainer.innerHTML += `
+            <div>
+             <p>There is no product to show.</p>
+            </div>
         `
-        });
-        
     }
-    
+    showTotal();
 }
+//showing total cost
+function showTotal(){
+    let total = document.querySelector(".final-cost span");
+    let getProducts = localStorage.getItem("productInCart");
+    getProducts = JSON.parse(getProducts);
+    if(getProducts == null && total){
+        total.textContent = "0.00 Tk";
+    }
+    else{
+        let currentTotal = localStorage.getItem("totalprice");
+        currentTotal = parseInt(currentTotal);
+        total.textContent = currentTotal;
+    }
+}
+//increase for quantity
+function increaseOne(k){
+    let getProducts = localStorage.getItem("productInCart");
+    getProducts = JSON.parse(getProducts);
+
+    let objectKey=[];
+      Object.keys(getProducts).map(key =>{
+          objectKey.push(key);
+      })
+
+    let sentItem = getProducts[objectKey[k]];
+    cartNumbers(sentItem);
+    totalCost(sentItem);
+    displayCart();
+}
+
+//decrease for quantity
+function decreaseOne(j){
+  let idtake = document.getElementById("quan-number");
+  let update = idtake.textContent;
+  update = parseInt(update);
+  let proNumbers = localStorage.getItem('cartTotal');
+  proNumbers = parseInt(proNumbers);
+  let getProducts = localStorage.getItem("productInCart");
+  getProducts = JSON.parse(getProducts);
+    let objectKey=[];
+      Object.keys(getProducts).map(key =>{
+          objectKey.push(key);
+      })
+  let sentItem = getProducts[objectKey[j]];
+  let currentItem = getProducts[objectKey[j]].inCart;  
+  currentItem -= 1;
+  check = update-1;
+  if(check <= 0 && currentItem<=0){
+      alert("minimum product must be one");
+      idtake.textContent = 1;
+      //document.getElementById("dec").style.color = "lightgrey";
+  }
+  else{
+    idtake.textContent = update-1; 
+    localStorage.setItem('cartTotal', proNumbers -1);
+    document.querySelector('#cart-bag span').textContent = proNumbers -1;
+      getProducts[objectKey[j]].inCart = currentItem;
+      localStorage.setItem("productInCart",JSON.stringify(getProducts));
+  }
+  displayCart();
+  costdec(sentItem);
+  onloadCartTotal();
+}
+//decreasing cart number
+function decreaseCartNumber(currentItem){
+    let proNumbers = localStorage.getItem('cartTotal');
+    proNumbers = parseInt(proNumbers);
+    //console.log(proNumbers);
+    localStorage.setItem('cartTotal', proNumbers -currentItem);
+    document.querySelector('#cart-bag span').textContent = proNumbers -currentItem; 
+}
+//decrease total cost
+function costcalculation(sentItem){
+    //console.log(typeof sentItem.inCart, typeof sentItem.price);
+    let costCurrent = sentItem.price * sentItem.inCart;
+    let costOld = localStorage.getItem("totalprice");
+    costOld = parseInt(costOld);
+    let costNew = costOld - costCurrent;
+    
+    localStorage.setItem("totalprice",costNew);
+}
+//decrease one productcost
+function costdec(sentItem){
+    let costOld = localStorage.getItem("totalprice");
+    costOld = parseInt(costOld);
+    let costCurrent = sentItem.price;
+    let costNew = costOld - costCurrent;
+    localStorage.setItem("totalprice",costNew);
+}
+//delete entire product
+function deleteitems(i){
+    let getProducts = localStorage.getItem("productInCart");
+    getProducts = JSON.parse(getProducts);
+     //delete program
+       let objectKey=[];
+      Object.keys(getProducts).map(key =>{
+          objectKey.push(key);
+      })
+      let sentItem = getProducts[objectKey[i]];
+      let currentItem = getProducts[objectKey[i]].inCart;
+      delete getProducts[objectKey[i]];
+      localStorage.setItem("productInCart",JSON.stringify(getProducts));
+      //delete ends
+      //cost calculation
+      decreaseCartNumber(currentItem);
+      onloadCartTotal();
+      costcalculation(sentItem);
+      displayCart();
+     
+}
+function clearAll(){
+        localStorage.clear();
+}
+//clearAll()
 onloadCartTotal();
 displayCart();
